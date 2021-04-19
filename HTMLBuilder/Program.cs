4a27d659a8a692acfba06bc80c7c1531d19a7186
Norah <Norahmutairi@outlook.sa>
2021-04-19 06:12:17 +0300

    Add project files.

---
+++ HTMLBuilder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HTMLBuilder
+{
+    class Program
+    {
+        class HTMLBuilder
+        {
+            public string html;
+
+            public HTMLBuilder OpenTag(string tag)
+            {
+                html += "<" + tag + ">";
+                return this;
+            }
+
+            public HTMLBuilder Body(string body)
+            {
+                html += body;
+                return this;
+
+            }
+
+            public HTMLBuilder ClosingTag(string tag)
+            {
+                html += "</" + tag + ">";
+                return this;
+
+            }
+
+            public string Get()
+            {
+                return html;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            HTMLBuilder myHtmlPage = new HTMLBuilder();
+
+            myHtmlPage.OpenTag("div").Body("Welcome to C#").ClosingTag("div");
+            Console.WriteLine(myHtmlPage.Get());
+        }
+    }
+}
